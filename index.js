d9399120dd996308c57a694e664e776f0f1422c6
Harshchafle <chafleharsh@gmail.com>
2024-12-22 18:57:44 +0530

    github Data Added

--- index.js
+++ index.js
@@ -3,7 +3,42 @@ require ('dotenv').config
 
 const express = require('express')
 const app = express()
-const port = 4000
+const port = 5000
+const githubData = {
+  "login": "Harshchafle",
+  "id": 157059028,
+  "node_id": "U_kgDOCVyH1A",
+  "avatar_url": "https://avatars.githubusercontent.com/u/157059028?v=4",
+  "gravatar_id": "",
+  "url": "https://api.github.com/users/Harshchafle",
+  "html_url": "https://github.com/Harshchafle",
+  "followers_url": "https://api.github.com/users/Harshchafle/followers",
+  "following_url": "https://api.github.com/users/Harshchafle/following{/other_user}",
+  "gists_url": "https://api.github.com/users/Harshchafle/gists{/gist_id}",
+  "starred_url": "https://api.github.com/users/Harshchafle/starred{/owner}{/repo}",
+  "subscriptions_url": "https://api.github.com/users/Harshchafle/subscriptions",
+  "organizations_url": "https://api.github.com/users/Harshchafle/orgs",
+  "repos_url": "https://api.github.com/users/Harshchafle/repos",
+  "events_url": "https://api.github.com/users/Harshchafle/events{/privacy}",
+  "received_events_url": "https://api.github.com/users/Harshchafle/received_events",
+  "type": "User",
+  "user_view_type": "public",
+  "site_admin": false,
+  "name": "Harsh Chafle",
+  "company": null,
+  "blog": "",
+  "location": "Nagpur, Maharashtra, India",
+  "email": null,
+  "hireable": null,
+  "bio": "👨‍💻 Welcome to my coding journey! 🚀\r\n🌟 I'm a passionate Software Developer driven by curiosity and a thirst for knowledge. 📚",
+  "twitter_username": null,
+  "public_repos": 21,
+  "public_gists": 0,
+  "followers": 18,
+  "following": 18,
+  "created_at": "2024-01-18T19:07:18Z",
+  "updated_at": "2024-12-22T12:58:38Z"
+}
 
 app.get('/', (req, res) =>{
     res.send('Hello World')
@@ -17,7 +52,11 @@ app.get('/login', (req, res) => {
     res.send("this is login Page")
 })
 
+app.get('/github', (req, res) => {
+    res.json(githubData)
+})
+
 // app.listen
 app.listen(process.env.PORT, () => {
-    console.log(`App Listening on port ${port} and ${process.env.PORT}`)
+    console.log(`App Listening on port ${port}`)
 })
